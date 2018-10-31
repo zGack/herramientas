aebc6db7260de6df6e1d1dd68af16ab050444e45
sebasmf99 <smf-mena@hotmail.com>
2018-10-30 22:46:59 -0500

    inicializacion

--- Herramientas.py
+++ Herramientas.py
@@ -1,5 +1,5 @@
 def operacion():
-    opt = str(input("Que opcion desea realizar?[suma - resta]: "))
+    opt = str(input("Que opcion desea realizar?[suma - resta - multiplicacion - division]: "))
     n1 = int(input("Digite el primer numero; "))
     n2 = int(input("Digite el segundo numero; "))
 
@@ -9,12 +9,21 @@ def operacion():
     elif(opt == "resta"):
         resultado = n1 - n2
         return resultado
+    elif (opt == "multiplicacion"):
+        resultado = n1 * n2
+        return resultado
+    elif (opt == "division"):
+        if(n2 > 0):
+            resultado = n1 / n2
+            return resultado
+        else:
+            return False
     else:
         return False
 
 res = operacion()
 
 if(res != False):
-    print('El resultado de la operacion es:', res)
+    print('El resultado de la operacion es:', int(res))
 else:
     print("Error, intentelo nuevamente")
